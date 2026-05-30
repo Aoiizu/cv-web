6f3dc8adc3f3d91c16d3c428aecf7113ee2f3d03
Aoiizu <alifahnafh3@gmail.com>
2026-05-31 02:00:51 +0900

    JS Update

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js'
+import { supabase } from './supabase.js'
 
 const supabaseUrl = 'https://hwuuwypxssfztpacxjke.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3dXV3eXB4c3NmenRwYWN4amtlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4OTMzNDcsImV4cCI6MjA5NTQ2OTM0N30.X21WO_wyIi1RtwF-2vaIU2jAQgVcescI5pSrzSEpizw'
